0362e7c9b4b65b0f58ae4ad00608fb37e8f73a27
Davi_Tolen <davitolen@outlook.com>
2021-10-13 15:04:55 -0300

    Desenvolvendo pagina cadastro
    Atualizando a pagina de cadastro

--- trainee_front_end/src/pages/Cadastro/Cadastro.js
+++ trainee_front_end/src/pages/Cadastro/Cadastro.js
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button, List, ListItem, ListItemText, Row, Col, Typography, ListGroupItem } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 import "./Cadastro.css";
 // import {IconContext} from "react-icons/lib";
 // import {IoMdSend} from "react-icons/io";
 
 function Cadastro(){
+    const [nome, setNome] = useState();
+    const [nascimento, setNascimento] = useState();
+    const [telefone, setTelefone] = useState();
+    const [email, setEmail] = useState();
+    const [endereco, setEndereco] = useState();
+    const [senha, setSenha] = useState();
+    const history = useHistory();
+
+    function login(){
+        alert("Bem Vindo" + nome);
+        history.push("login")
+    }
+
     return (
         <div>
-            <div className="containerCadastroDiv">
-                <div className="imagem">
-                    <img className="logo" src="/images/hospitaldaher.jpg" alt="logo" width="150px" height="150px"></img>
+            <div className="titlecadastro">
+                    <div className="imagem">
+                        <img className="logo" src="/images/hospitaldaher.jpg" alt="logo" width="150px" height="150px"></img>
+                    </div>
+                    <h2>Cadastro</h2>
                 </div>
-            </div>
         <div className="base">
             <div className="container">
-                <div className="titlecadastro">
-                    <div className="localCadastro">
-                        <h2>Cadastro</h2>
-                    </div>
-                </div>
                 <div>
                     <Form className="dados">
                     <Row className="mb-3">
@@ -27,35 +37,36 @@ function Cadastro(){
                             required
                             type="text"
                             placeholder="Nome Completo"
+                            onChange={(e)=> setNome(e.target.value)}
                             />
                         </Form.Group>
                         </Row>
                         <Row className="mb-3">
                             <Form.Group as={Col} controlId="nascimento">
-                                <Form.Control type="birthday" placeholder="Nascimento" />
+                                <Form.Control type="birthday" placeholder="Nascimento" onChange={(e)=> setNascimento(e.target.value)} />
                             </Form.Group>
 
                             <Form.Group as={Col} controlId="telefone">
-                                <Form.Control type="phone" placeholder="Telefone" />
+                                <Form.Control type="phone" placeholder="Telefone" onChange={(e)=> setTelefone(e.target.value)}/>
                             </Form.Group>
                         </Row>
                         <Row className="mb-3">
                             <Form.Group controlId="endereço">
-                                <Form.Control type="address" placeholder="Endereço" />
+                                <Form.Control type="address" placeholder="Endereço" onChange={(e)=> setEndereco(e.target.value)}/>
                             </Form.Group>
                         </Row>
                         <Row className="mb-3">
                             <Form.Group controlId="email">
-                                <Form.Control type="email" placeholder="Endereço de email" />
+                                <Form.Control type="email" placeholder="Endereço de email" onChange={(e)=> setEmail(e.target.value)}/>
                             </Form.Group>
                         </Row>
                         <Row className="mb-3">
                             <Form.Group controlId="senha">
-                                <Form.Control type="password" placeholder="Senha"/>
+                                <Form.Control type="password" placeholder="Senha" onChange={(e)=> setSenha(e.target.value)}/>
                             </Form.Group>
                         </Row>
 
-                        <Button className="butao" variant="danger" type="submit" >
+                        <Button className="butao" variant="danger" type="submit" onClick={login}>
                         Enviar
                         </Button> 
 
