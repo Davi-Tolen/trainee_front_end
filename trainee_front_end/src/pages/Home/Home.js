3bff17003730950a79e9ecc90f80c7f5a93ca588
Adrianus Alexsander <adrianusvieira@cpejr.com.br>
2021-10-18 21:31:04 -0300

    Home2.0

--- trainee_front_end/src/pages/Home/Home.js
+++ trainee_front_end/src/pages/Home/Home.js
@@ -5,7 +5,11 @@ function Home () {
     return(
       <div className="home">
         <div className="entrada1"><img src="/images/entrada1.png" alt="entrada" className="entrada1"></img></div>
-        <div className="entrada2"><Button variant="outline-light">Light</Button></div>
+        <div className="entrada2"><Button variant="outline-light">Agende sua Consulta</Button></div>
+        <div className="agende">
+          <div className="agende1"><p className="atende-los">Estamos prontos para atende-lo com eficiência e conforto!</p></div>
+          <div className="agende2"><Button variant="outline-light">Agende sua Consulta</Button></div>
+        </div>
       </div>
     )
 };
