f535bae7e7d677c99f273e2f79a5a1a4130cfbef
Adrianus Alexsander <adrianusvieira@cpejr.com.br>
2021-10-21 16:21:25 -0300

    Nossos Profissionais
    Arrumando Rotas

--- trainee_front_end/src/routes.js
+++ trainee_front_end/src/routes.js
@@ -9,6 +9,7 @@ import Header from "./pages/Header";
 import PaginaInicial from "./pages/PaginaInicial";
 import NossosProfissionais from "./pages/NossosProfissionais";
 
+
 function Routes(){
     return(
         <BrowserRouter>
